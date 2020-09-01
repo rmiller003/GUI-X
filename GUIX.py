5efc278cdbf310bb2eb1ab4a6974fcdc03887321
rmill <rmiller003@gmail.com>
2020-09-01 01:44:07 -0400

    Stop = 7:20

--- GUIX.py
+++ GUIX.py
@@ -2,16 +2,39 @@
 # By RPM
 
 import turtle
+import time
+
+delay = 0.1
 
 # Set up the screen
 wn = turtle.Screen()
 wn.title("GUI-X Game by RPM")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
-wn.tracer(0)
+wn.tracer(0) # Turns off screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "up"
 
 
+# Functions
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
 
+# Main game loop
+while True:
+    wn.update()
 
+    move()
+
+    time.sleep(delay)
 
 wn.mainloop()
