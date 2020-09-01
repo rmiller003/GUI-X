ee2c062dcedb4de9c99d18d33a83af9701d7f2f7
rmill <rmiller003@gmail.com>
2020-09-01 01:28:04 -0400

    Initial commit

---
+++ GUIX.py
@@ -0,0 +1,17 @@
+# GUI-X is a simple Snake Game written in Python 3
+# By RPM
+
+import turtle
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("GUI-X Game by RPM")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+
+
+
+wn.mainloop()
